60827912f6e31d7eee317d3928f4826b3d607fdb
Oliver Großkloß <22076711+Progaros@users.noreply.github.com>
2025-06-05 16:53:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from nice_module import nice_function
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return nice_function()
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
